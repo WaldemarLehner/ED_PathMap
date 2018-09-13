6ee13ea37e62c35e910bb9049f9c6d427bb75d61
Waldemar L <wowa.lehner@gmail.com>
2018-09-13 12:52:11 +0200

    added a basic skybox. Nothing fancy, textures need redoing.

--- frontend/src/js/drawData.js
+++ frontend/src/js/drawData.js
@@ -9,17 +9,18 @@ function drawData(logList, systemList, connectionList, maxSystemVisitCount, maxC
 //#region Setup
 //Settings
   //#region SET BY USER//
+    // Show Debug Data
+  var _USER_DEBUG = true;
     // Should the points and lines have a identical color scale? Will use the highest value from maxSystemVisitCount / maxConnectionVisitCount
   var _USER_USE_IDENTICAL_SCALE = false;
     //Should the maxSystemVisitCount/maxConnectionVisitCount be overwritten?
   var _USER_OVERRIDE_MAX_COUNT = true;
   var _USER_OVERRIDE_DEFINITIONS = {system:50,connection:10};
-
   // Should point/line size be affected by times visited?
   var _USER_VISIT_AFFECT_POINT_SIZE = true;
   var _USER_VISIT_AFFECT_LINE_SIZE = true;
     // Min/max point/line size (default is used if _USER_VISIT_AFFECT_LINE/POINT_SIZE is set to "false")
-  var _SIZE_DEFINITIONS = {point:{min:2,max:5,default:3},line:{min:1,max:10,default:2}};
+  var _SIZE_DEFINITIONS = {point:{min:2,max:4,default:3},line:{min:1,max:10,default:2}};
 
     // Color values (in hexdec) for maximum and minimum values
   var _COLOR_DEFINITIONS = {min:"#a30000",max:"#00ff00"};
@@ -44,11 +45,14 @@ function drawData(logList, systemList, connectionList, maxSystemVisitCount, maxC
   }
 _systemList = systemList;
 _connectionList = connectionList;
+  var isDefaultSceneDrawn = true;
 //Settings up canvas end adding it to the HTML document
+  var scene_skybox = new THREE.Scene();
   var scene_main = new THREE.Scene();
   var camera = new THREE.PerspectiveCamera( 80, canvas_width/canvas_height,0.1,500000);
   camera.position.set(0.5,0.2,0.1);
   var renderer = new THREE.WebGLRenderer();
+  renderer.autoClear = false;
   renderer.setSize(canvas_width,canvas_height);
   $("#3d_container").append(renderer.domElement);
 
@@ -84,19 +88,17 @@ else if(typeof maxConnectionVisitCount !== "number"){
 }
 //#endregion
 //#region Draw Connection Lines
-  for(var entry in connectionList){
+  for(let entry in connectionList){
     try{
       //skip loop if property is from prototype
       if(!connectionList.hasOwnProperty(entry)){continue;}
-      var material = getMaterialByCount(connectionList[entry].tosys1count+connectionList[entry].tosys2count,false);
-      var geometry = new THREE.Geometry();
-      var system1 = systemList[connectionList[entry].sys1];
-      //console.log(systemList);
-      //console.log(connectionList);
-      //console.log(connectionList[entry]);
-      var system2 = systemList[connectionList[entry].sys2];
-      geometry.vertices.push(new THREE.Vector3(system1.x,system1.y,system1.z),new THREE.Vector3(system2.x,system2.y,system2.z));
-      var object = new THREE.Line(geometry,material);
+      let material = getMaterialByCount(connectionList[entry].tosys1count+connectionList[entry].tosys2count,false);
+      let geometry = new THREE.BufferGeometry();
+      let system1 = systemList[connectionList[entry].sys1];
+      let system2 = systemList[connectionList[entry].sys2];
+      let vertices = new Float32Array([system1.x,system1.y,system1.z,system2.x,system2.y,system2.z]);
+      geometry.addAttribute("position",new THREE.BufferAttribute(vertices,3));
+      let object = new THREE.Line(geometry,material);
       object.name = entry;
       scene_main.add(object);
     }
@@ -109,21 +111,17 @@ else if(typeof maxConnectionVisitCount !== "number"){
 //#endregion
 //#region Draw System Dots
 
-  for(var entry in systemList){
+  for(let entry in systemList){
 
     //skip loop if property is from prototype
       if(!systemList.hasOwnProperty(entry)){ console.warn(entry);continue; }
-      var material = getMaterialByCount(systemList[entry].count,true);
-      var geometry = new THREE.Geometry();
-      geometry.vertices.push(new THREE.Vector3(
-        systemList[entry].x,
-        systemList[entry].y,
-        systemList[entry].z
-      ));
-      var object = new THREE.Points(geometry,material);
+      let material = getMaterialByCount(systemList[entry].count,true);
+      let geometry = new THREE.BufferGeometry();
+      let vertices = new Float32Array([systemList[entry].x,systemList[entry].y,systemList[entry].z]);
+      geometry.addAttribute("position",new THREE.BufferAttribute(vertices,3));
+      let object = new THREE.Points(geometry,material);
       object.name = systemList[entry].name;
       scene_main.add(object);
-      //console.log(object);
   }
 
 
@@ -142,7 +140,22 @@ else if(typeof maxConnectionVisitCount !== "number"){
       else{
         size = _SIZE_DEFINITIONS.point.default;
       }
-      return new THREE.PointsMaterial({color:color,size:size});
+      //return new THREE.PointsMaterial({color:color,size:size});
+      //Generate Canvas for round dot
+      let c = document.createElement("canvas");
+      let ctx_size = 20;
+      c.width = c.height = ctx_size;
+      let ctx = c.getContext("2d");
+      let texture = new THREE.Texture(c);
+      ctx.beginPath();
+      ctx.arc(ctx_size/2,ctx_size/2,ctx_size/2,0,2*Math.PI,false);
+      ctx.fillStyle = color;
+      ctx.fill();
+      texture.needsUpdate = true;
+      if(texture.minFilter !== THREE.NearestFilter && texture.minFilter !== THREE.LinearFilter){
+        texture.minFilter = THREE.NearestFilter;
+      }
+      return new THREE.PointsMaterial({size:size,map:texture,transparent:true,depthWrite:false});
     }
     else{
       //Line handling
@@ -159,9 +172,24 @@ else if(typeof maxConnectionVisitCount !== "number"){
     }
   }
 //#endregion
+//#region Skybox
+let skybox_material_data = [
+  new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("src/img/skybox/north.png"),side:THREE.DoubleSide}),
+  new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("src/img/skybox/south.png"),side:THREE.DoubleSide}),
+  new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("src/img/skybox/up.png"),side:THREE.DoubleSide}),
+  new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("src/img/skybox/down.png"),side:THREE.DoubleSide}),
+  new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("src/img/skybox/east.png"),side:THREE.DoubleSide}),
+  new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("src/img/skybox/west.png"),side:THREE.DoubleSide}),
+];
+
+let skybox = new THREE.Mesh(new THREE.CubeGeometry(100000,100000,100000),skybox_material_data);
+console.log(skybox_material_data);
+scene_skybox.add(skybox);
+scene_skybox.add(new THREE.AmbientLight(0xFFFFFF,0.3));
+//#endregion
 //#region Controls
 //Controls Setup
-var controls = new THREE.OrbitControls( camera );
+let controls = new THREE.OrbitControls( camera );
 controls.enableDamping = true;
 controls.dampingFactor = 1.5;
 controls.keys = {
@@ -173,8 +201,18 @@ console.log(controls);
 //#region animation loop at the end of loading
 function animate(){
   requestAnimationFrame(animate);
-  renderer.render(scene_main,camera);
-  controls.update();
+  update();
+  if(isDefaultSceneDrawn){
+    renderer.clear();
+    renderer.render(scene_skybox,camera);
+    renderer.clearDepth();
+    renderer.render(scene_main,camera);
+    controls.update();
+  }
+}
+function update(){
+  skybox.position.set(camera.position.x,camera.position.y,camera.position.z) ;
+  console.log(camera.position);
 }
 animate();
 //#endregion
