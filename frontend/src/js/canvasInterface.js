2e41a36ead285ae845f12232fc03dfc4012c7220
Waldemar L <wowa.lehner@gmail.com>
2018-10-26 15:01:59 +0200

    Added system Name UI

--- frontend/src/js/canvasInterface.js
+++ frontend/src/js/canvasInterface.js
@@ -47,6 +47,15 @@ PATHMAP.Interface = function(camera, scenes, controls, linesref, pointsref, logL
 	this.getControls = function() {
 		return _controls;
 	};
+	this.getSystemInFocus = function() {
+		return getSystemByIndex(_currentIndex);
+	};
+	this.killSystemUI = function() {
+		if (typeof _sysUI !== "undefined") {
+			_scenes[2].remove(_scenes[2].getObjectByName("systemInfo"));
+			_sysUI = undefined;
+		}
+	};
 	//#endregion
 	//#region SET
 	this.addMarker = function(vPos, marker, returnMarker) {
@@ -103,26 +112,42 @@ PATHMAP.Interface = function(camera, scenes, controls, linesref, pointsref, logL
 	};
 	this.focus = {
 		current: function(showSystemData) {
-			focusAtSystem(getSystemByIndex(_currentIndex));
+			focusAtSystem(getSystemByIndex(_currentIndex), showSystemData);
 		},
 		next: function(showSystemData) {
 			_currentIndex = (_currentIndex < _logList.length - 1) ? (_currentIndex = _currentIndex++) : (_logList.length - 1);
 			_currentIndex++;
-			focusAtSystem(getSystemByIndex(_currentIndex));
+			focusAtSystem(getSystemByIndex(_currentIndex), showSystemData);
 		},
 		previous: function(showSystemData) {
 			_currentIndex = (_currentIndex > 0) ? (_currentIndex - 1) : (0);
-			focusAtSystem(getSystemByIndex(_currentIndex));
+			focusAtSystem(getSystemByIndex(_currentIndex), showSystemData);
 		},
 		first: function(showSystemData) {
 			_currentIndex = 0;
-			focusAtSystem(getSystemByIndex(_currentIndex));
+			focusAtSystem(getSystemByIndex(_currentIndex), showSystemData);
 		},
 		last: function(showSystemData) {
 			_currentIndex = _logList.length - 1;
-			focusAtSystem(getSystemByIndex(_currentIndex));
+			focusAtSystem(getSystemByIndex(_currentIndex), showSystemData);
+		},
+		nth: function(index,showSystemData) {
+			if(typeof index !== "number"){
+				throw "first parameter needs to be a number.";
+			}
+			let logLength = _logList.length;
+			if(index > _logList.length-1){
+				console.warn("Index was greater than logList length and has been clamped.");
+				index = _logList.length-1;
+			}else if(index < 0){
+				console.warn("Index may not be smaller than 0 and therefore has been clamped up to 0.");
+				index = 0;
+			}
+			_currentIndex = index;
+			focusAtSystem(getSystemByIndex(_currentIndex), showSystemData);
+
 		}
-		// TODO... should player be added here?
+		//TODO ... should player be added here?
 	};
 	//#endregion
 	this.showSystemDots = function(bool) {
@@ -177,11 +202,90 @@ PATHMAP.Interface = function(camera, scenes, controls, linesref, pointsref, logL
 		return object;
 	}
 
-	function focusAtSystem(system) {
+	function generateSystemInfo(sysInfo, returnObj) {
+		let canvas = document.createElement("canvas");
+		canvas.width = 512;
+		canvas.height = 128;
+		let ctx = canvas.getContext("2d");
+		let texture = new THREE.Texture(canvas);
+		let fontSize1 = 40;
+		let fontSize2 = 25;
+		let convergence = 15;
+		let backgrd_margin = 10;
+		ctx.globalAlpha = 0.6;
+		roundRect(14,backgrd_margin+4,canvas.width-14,canvas.height-backgrd_margin-4,16,"black",false);
+		ctx.globalAlpha = 1;
+		roundRect(14,backgrd_margin+4,canvas.width-14,canvas.height-backgrd_margin-4,16,"orange",true);
+		ctx.font = fontSize1 + "px Calibri";
+		ctx.textAlign = "left";
+		ctx.fillStyle = "#ff7100";
+		let visitText = (sysInfo.count == 1) ? " visit" : " visits";
+
+		ctx.fillText(sysInfo.name, 25, fontSize1 + convergence, canvas.width - 40);
+		ctx.font = fontSize2 + "px sans-serif";
+		ctx.fillStyle = "#b34f00";
+		ctx.fillText(sysInfo.count + visitText, 25, canvas.height / 2 + fontSize2 + 20 - convergence, 100);
+		ctx.fillText(sysInfo.date,25+100+10,canvas.height/2+fontSize2+20-convergence,canvas.width-135);
+		//draw little triangle pointing at system.
+		ctx.beginPath();
+		ctx.moveTo(0,canvas.height/2);
+		ctx.lineTo(14,canvas.height/2-15);
+		ctx.lineTo(14,canvas.height/2+15);
+		ctx.lineTo(0,canvas.height/2);
+		ctx.globalAlpha = 0.5;
+		ctx.fillStyle = "orange";
+		ctx.fill();
+		texture.needsUpdate = true;
+		let material = new THREE.SpriteMaterial({
+			map: texture,
+			color: 0xFFFFFF
+		});
+		material.sizeAttenuation = false;
+		let sysText = new THREE.Sprite(material);
+		sysText.position.set(-sysInfo.coords.x, sysInfo.coords.y, sysInfo.coords.z);
+		sysText.center.x = -0.05;
+		let sizeDivider = 7.5;
+		sysText.scale.set(4 / sizeDivider, 1 / sizeDivider, 1);
+		sysText.name = "systemInfo";
+		_sysUI = sysText;
+		_scenes[2].add(sysText);
+		return (returnObj) ? _sysUI : _this;
+
+		function roundRect(x0, y0, x1, y1, r, color,makeStroke) {
+			var w = x1 - x0;
+			var h = y1 - y0;
+			if (r > w / 2) r = w / 2;
+			if (r > h / 2) r = h / 2;
+			ctx.beginPath();
+			ctx.moveTo(x1 - r, y0);
+			ctx.quadraticCurveTo(x1, y0, x1, y0 + r);
+			ctx.lineTo(x1, y1 - r);
+			ctx.quadraticCurveTo(x1, y1, x1 - r, y1);
+			ctx.lineTo(x0 + r, y1);
+			ctx.quadraticCurveTo(x0, y1, x0, y1 - r);
+			ctx.lineTo(x0, y0 + r);
+			ctx.quadraticCurveTo(x0, y0, x0 + r, y0);
+			ctx.closePath();
+			if(makeStroke){
+				ctx.strokeStyle = color;
+				ctx.stroke();
+			}else{
+				ctx.fillStyle = color;
+				ctx.fill();
+			}
+		}
+	}
+
+	function focusAtSystem(system, showSystemInfo) {
 		console.log(system);
-		let directionCamera_System = new THREE.Vector3(_camera.position.x-system.coords.x,_camera.position.y-system.coords.y,_camera.position.z-system.coords.z);
-		directionCamera_System.clampLength(100,10000);
-		_this.focusCamera(new THREE.Vector3(system.coords.x,system.coords.y,system.coords.z),directionCamera_System.length(),undefined,1000);
+		let directionCamera_System = new THREE.Vector3(_camera.position.x - system.coords.x, _camera.position.y - system.coords.y, _camera.position.z - system.coords.z);
+		directionCamera_System.clampLength(100, 10000);
+
+		_this.focusCamera(new THREE.Vector3(system.coords.x, system.coords.y, system.coords.z), directionCamera_System.length(), undefined, 1000);
+		if (showSystemInfo) {
+			_this.killSystemUI();
+			return generateSystemInfo(system);
+		}
 	}
 	//#endregion
 	//#endregion
@@ -196,7 +300,6 @@ PATHMAP.Interface = function(camera, scenes, controls, linesref, pointsref, logL
 	var _currentIndex = 0;
 	var _logList = logList;
 	var _sysList = sysList;
-
 	var _services = {};
 	//#endregion
 };
